1e5116238e71bcaab5904f6305dafeba2518f2c3
zora santos <zoranildo22@gmail.com>
2022-02-28 01:20:55 -0300

    implements database and orm

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from '../../repositories/implementations/Categor
 import { ListCategoriesController } from './ListCategoriesController';
 import { ListCategoryUseCase } from './ListCategoryUseCase';
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const listCategoryUseCase = new ListCategoryUseCase(categoriesRepository);
 const listCategoriesController = new ListCategoriesController(listCategoryUseCase);
 
