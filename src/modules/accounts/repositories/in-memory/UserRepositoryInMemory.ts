97bc384ded1ceb4762df2bf7886458c218d53419
zora santos <zoranildo22@gmail.com>
2022-03-09 08:04:36 -0300

    create test authenticate user

---
+++ src/modules/accounts/repositories/in-memory/UserRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UserRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+  async create({ driver_license, name, email, password }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      name,
+      email,
+      password
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UserRepositoryInMemory };
