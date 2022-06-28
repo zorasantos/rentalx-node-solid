4b0eb12d1b33243a14e4f66aeee1e7c690aee2cc
zora santos <zoranildo22@gmail.com>
2022-06-28 07:16:28 -0300

    chore: create middleware user admin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,16 +3,12 @@ import { Router } from 'express';
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post('/', createSpecificationController.handle);
-
-// categoriesRoutes.get('/list', (req, res) => {
-//   const all = categoriesRepository.list();
-//   return res.json(all);
-// });
+specificationsRoutes.post('/', ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 export { specificationsRoutes };
